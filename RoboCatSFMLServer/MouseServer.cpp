4408129f939554cf9785ae8eec48045072999032
Ethan Reilly <ethanreilly4@gmail.com>
2023-05-17 11:46:44 +0100

    Fixed scoreboard changing to match health

--- RoboCatSFMLServer/MouseServer.cpp
+++ RoboCatSFMLServer/MouseServer.cpp
@@ -17,6 +17,8 @@ bool MouseServer::HandleCollisionWithCat(RoboCat* inCat)
 	// Change so when you collect pickup, health increases
 	inCat->GetHealth()++;
 	//inCat->GetHealth() + 1;
+	int ECRS_Health = 1 << 3;
+	NetworkManagerServer::sInstance->SetStateDirty(inCat->GetNetworkId(), ECRS_Health);
 	
 	//kill yourself!    :(
 	SetDoesWantToDie(true);
