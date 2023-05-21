1efa3c34d08cecd010b2c8bf8d6542622bc8aa3b
Ethan Reilly <ethanreilly4@gmail.com>
2023-05-21 14:07:58 +0100

    Editing Burger/Yarn damage

--- RoboCatSFMLServer/YarnServer.cpp
+++ RoboCatSFMLServer/YarnServer.cpp
@@ -31,7 +31,7 @@ bool YarnServer::HandleCollisionWithCat(RoboCat* inCat)
 		//kill yourself!
 		SetDoesWantToDie(true);
 
-		static_cast<RoboCatServer*>(inCat)->TakeDamage(GetPlayerId(), 3);
+		static_cast<RoboCatServer*>(inCat)->TakeDamage(GetPlayerId(), 1);
 
 	}
 
